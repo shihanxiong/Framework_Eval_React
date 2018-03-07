8e92fb333be03512d2860fc39fbc178ad2179f7e
shihanxiong <shihanxiong@gmail.com>
2018-03-07 13:29:27 -0500

    update.

--- booklist/src/index.js
+++ booklist/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css'
+
+import rootReducers from './reducers/index';
+
+const store = createStore(rootReducers);
+
+ReactDOM.render(    <Provider store={store}>
+                        <App />
+                    </Provider> , 
+                    document.getElementById('root'));
+
 registerServiceWorker();
--- booklist/src/reducers/index.js
+++ booklist/src/reducers/index.js
@@ -2,8 +2,10 @@ import {
     combineReducers
 } from 'redux';
 
+import BooksReducer from './reducer_books';
+
 const rootReducer = combineReducers({
-    state: (state = {}) => state
+    books: BooksReducer
 });
 
 export default rootReducer;
