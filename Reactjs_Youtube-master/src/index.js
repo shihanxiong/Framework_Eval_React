1679db78a56bda2e6ed56df5648159c44a59294c
Hanxiong Shi <hshi@trellisrx.com>
2018-07-20 13:32:32 -0400

    update.

--- Reactjs_Youtube-master/src/index.js
+++ Reactjs_Youtube-master/src/index.js
@@ -8,40 +8,43 @@ import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyC4M3hZUnO6yyMQDUBNQx1veCoUWAB6K0w';
 
 class App extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = { 
-            videos: [],
-            selectedVideo: null 
-        };
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
 
-        this.videoSearch('surfboards');
-    }
+    this.videoSearch('surfboards');
+  }
 
-    videoSearch(term) {
-        YTSearch({key: API_KEY, term: term}, (videos) => {
-            this.setState({ 
-                videos: videos,
-                selectedVideo: videos[0]
-            });
-            // ES6 -> this.setState({ videos });
-        });
-    }
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0],
+      });
+      // ES6 -> this.setState({ videos });
+    });
+  }
 
-    render() {
-        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300); // trigger function every 300ms
+  render() {
+    const videoSearch = _.debounce((term) => {
+      this.videoSearch(term);
+    }, 300); // trigger function every 300ms
 
-        return (
-            <div>
-                <SearchBar onSearchTermChange={videoSearch} />
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList
-                    onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
-                    videos={this.state.videos} />
-            </div>
-        );
-    }
+    return (
+      <div>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={(selectedVideo) => this.setState({ selectedVideo })}
+          videos={this.state.videos}
+        />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
--- Reactjs_Youtube-master/test/test_helper.js
+++ Reactjs_Youtube-master/test/test_helper.js
@@ -17,10 +17,10 @@ const $ = _$(window);
 chaiJquery(chai, chai.util, $);
 
 function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
+  const componentInstance = TestUtils.renderIntoDocument(
     <Provider store={createStore(reducers, state)}>
       <ComponentClass {...props} />
-    </Provider>
+    </Provider>,
   );
 
   return $(ReactDOM.findDOMNode(componentInstance));
@@ -33,4 +33,4 @@ $.fn.simulate = function(eventName, value) {
   TestUtils.Simulate[eventName](this[0]);
 };
 
-export {renderComponent, expect};
+export { renderComponent, expect };
