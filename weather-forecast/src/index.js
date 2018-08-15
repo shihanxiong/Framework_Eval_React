2dd5ab10434865cf5280a360f95996bb797b1e5e
shihanxiong <shihanxiong@gmail.com>
2018-08-15 00:26:39 -0400

    update.

--- weather-forecast/src/index.js
+++ weather-forecast/src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ReduxPromise from 'redux-promise';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
