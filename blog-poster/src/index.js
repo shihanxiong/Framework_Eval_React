f7e9a2538751fa88a54b04c335e642daaefedc62
shihanxiong <shihanxiong@gmail.com>
2018-08-19 22:52:25 -0400

    update.

--- blog-poster/src/index.js
+++ blog-poster/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
+import promise from 'redux-promise';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducers from './reducers';
+import PostsIndex from './components/posts_index';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root'),
+);
 registerServiceWorker();
