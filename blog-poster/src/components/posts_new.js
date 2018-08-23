3d0d9c5d4783071c2b21c72838e5f18520c1d11b
shihanxiong <shihanxiong@gmail.com>
2018-08-23 14:34:26 -0400

    Added form validation.

--- blog-poster/src/components/posts_new.js
+++ blog-poster/src/components/posts_new.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+        {field.meta.error}
+      </div>
+    );
+  }
+
   render() {
-    return <div>PostsNew</div>;
+    return (
+      <form>
+        <Field label="Title" name="title" component={this.renderField} />
+        <Field
+          label="Categories"
+          name="categories"
+          component={this.renderField}
+        />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
+    );
   }
 }
 
-export default PostsNew;
+function validate(values) {
+  // console.log(values) -> { title: 'asdf', categories: 'asdf', content: 'asdf' }
+  const errors = {};
+
+  // validate the inputs from 'values'
+  if (!values.title || values.title.length < 3) {
+    // 'title' property has to match field.name property
+    errors.title = 'Enter a title that is at least 3 characters!';
+  }
+  if (!values.categories) {
+    errors.categories = 'Enter some categories';
+  }
+  if (!values.content) {
+    errors.content = 'Enter some content please';
+  }
+
+  // If errors is empty, the form is fine to submit
+  // If errors has any properties, redux form assumes form is invalid
+
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: 'PostsNewForm',
+})(PostsNew);
