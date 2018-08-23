ed7341ac6ecbc98d1800d30cce15332e0d53d25b
shihanxiong <shihanxiong@gmail.com>
2018-08-23 16:11:34 -0400

    update.

--- blog-poster/src/index.js
+++ blog-poster/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import './index.css';
+import './style/style.css';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import promise from 'redux-promise';
